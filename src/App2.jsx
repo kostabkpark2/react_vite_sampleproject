1bc4f7931cb259a86ab6528e0c1497ce5b3946d2
kostabkpark2 <kostabkpark2@gmail.com>
2025-11-05 15:41:08 +0900

    useReducer - 2

--- src/App2.jsx
+++ src/App2.jsx
@@ -6,9 +6,13 @@ import Button from './Button.jsx'
 // dispatcher 가 reducer 에게 시키는 일(Action)을 상세히 적는것
 const reducer = (state, action) => {
   switch(action.type) {
-    case "UP" : console.log("10씩 증가"); break;
-    case "DOWN" : console.log("10씩 감소"); break;
-    default : return;
+    case "UP" : 
+      console.log("10씩 증가"); 
+      return state + action.payload ;
+    case "DOWN" : 
+      console.log("10씩 감소"); 
+      return state - action.payload ;
+    default : return state;
   }
 // "up" : counter(state) 를 payload 만큼 증가
 // "down" : counter(state) 를 payload 만큼 감소
@@ -16,26 +20,12 @@ const reducer = (state, action) => {
 
 
 function App() {
-  const [counter, setCounter] = useState(0);
   const [numberTotal, dispatch] = useReducer(reducer, 0);
   
-  const onClick = () => {
-    setCounter((counter) => {
-      return counter = counter + 1 ;
-    });
-  }
-  const func = () => {
-    console.log("화면이 최초 렌더링 되었습니다. -- componentDidMount")
-  };
-  const func2 = () => {
-    console.log("화면이 업데이트 되었습니다. -- componentDidUpdate")
-  };
-  useEffect(func, [] );  // componentDidMount() ;
-  useEffect(func2, [counter] );  // componentDidUpdate() ;
   return (
       <div>
         <h1>Class Component</h1>
-        <p>{counter}</p>
+        <p>{numberTotal}</p>
         <Button onClick={()=>{
           dispatch({type : "UP", payload : 10})
         }} text="Up" />
